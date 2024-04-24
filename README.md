d25250519b8e71be77bd7ab6d1e2fd3a971a7d6d
Janne Järvenpää <janne.jarvenpaa@gmail.com>
2024-04-24 23:04:11 +0300

    Updated readme, art, colors, arch, BT, commands HW, OS

--- README.md
+++ README.md
@@ -1,7 +1,41 @@
 # raspi-welcome
-My lightweight Raspberry welcome script
+
+This (welcome.sh) is my default welcome message especially for Raspberry Pi devices. The script provides useful information and can help prevent confusion or misremembering some things when using multiple similar devices. It also shows the temperature in Raspberry Pi devices, which can unexpectedly rise when the load is increased, for example due to the wrong type of case.
+
+## Routines
+
+The script contains the following routines:
 - checks internet connection when connected locally
 - displays CPU temperature (Raspberry OS only)
-- displays current time
-- displays ASCII art from text file
+- displays CPU architecture
+- displays SBC (Single-board computer) or CPU model
+- displays current local time
+- displays ASCII art
+- displays OS version
+- displays if bluetooth adapter is up or found
+- displays screen/windows size in characters
 - shows if specific commands are installed or not
+
+## Usage
+
+1) Copy the script into your user script directory
+2) Call it at the end of ~/.bashrc file to run it each time you login
+
+### Parameters
+
+To customize the default ASCII art just provide some text file path as only parameter.
+
+><><>< raspi-welcome v0.2 beta
+usage: welcome.sh ascii-art-file
+
+### Raspberry OSes
+
+The script is tested on Raspberry Pi Model B Rev 2.
+
+(image)
+
+### Linux Mint
+
+The script works also on most other Linux-gnu distros. Since supported commands varies a little the result information is also a bit different.
+
+(image)
--- welcome.sh
+++ welcome.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#
-#                  My Raspberry Pi welcome script
-#         Copyright (c) 2024 <janne.jarvenpaa@gmail.com>
-#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#
+#              My Raspberry Pi welcome script
+#         Copyright (c) 2024 <jarvenja@gmail.com>
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#
+
+checkBt () {
+	local bt dev
+	bt="Bluetooth"
+  	dev=$(hcitool dev | tr -d '\n' | sed 's/\t/ /g' | sed "s/^Devices://g")
+ 	[ -z "$dev" ] && echo "$(dim $bt:) No adapter found." || echo -e "\e[94m$bt:\e[39m $dev"
+ }
 
 checkTemp () {
 	local number temp
@@ -10,31 +17,44 @@ checkTemp () {
  	if [ -n "$temp" ]; then
 		number="${temp:5:-2}"
 		if [ -z $(command -v "bc") ]; then temp+=" (bc required for comparison)"
-		elif (( $(echo "$number > 65.0" | bc -l) )); then temp+=" is hot!"
-		elif (( $(echo "$number > 40.0" | bc -l) )); then temp+=" is warm."
-		else temp+=" is fine."
+		elif (( $(echo "$number > 65.0" | bc -l) )); then temp+=" is \e[33mhot!\e[39m"
+		elif (( $(echo "$number > 40.0" | bc -l) )); then temp+=" is \e[38;5;208mwarm\e[39m."
+		else temp+=" is \e[33mfine\e[39m."
 		fi
 	else
-		temp="(Temperature not available)"
+		temp=$(dim "temperature not available")
 	fi
-	echo "CPU $temp"
+	echo -e "$(dim CPU) $temp"
+}
+
+defaultAscii () {
+	cat << "***"
+ '|| '||'  '|'         '||                                    
+  '|. '|.  .'    ....   ||    ....    ...   .. .. ..     .... 
+   ||  ||  |   .|...||  ||  .|   '' .|  '|.  || || ||  .|...||
+    ||| |||    ||       ||  ||      ||   ||  || || ||  ||     
+     |   |      '|...' .||.  '|...'  '|..|' .|| || ||.  '|...'
+***
+}
+
+dim () { # str
+	echo -e "\e[2m${1}\e[0m"
 }
 
 displayArt () { # [txtFile]
 	local art
 	art="${1:-./$(hostname).txt}"
-	[ -f "$art" ] && cat "$art" || echo "*** [$art] ***"
- 	echo
+	[ -f "$art" ] && cat "$art" || defaultAscii
+	echo
 }
 
 displayCmdStatus () { # commands...
 	local installed missing
-	installed="Installed:"
-	missing="- Missing:"
 	for cmd in "$@"; do
 		[ $(command -v "$cmd") ] && installed+=" ${cmd}" || missing+=" ${cmd}"
 	done
-	printf "${installed}\n${missing}\n"
+	echo -e "$(dim 'Commands:')" $(green "${installed}")
+	echo -e "$(dim '-missing:')" $(red "${missing}")
 }
 
 displayConnection () {
@@ -45,7 +65,21 @@ displayConnection () {
 		resp=$(curl -Is http://www.google.com | head -n 1)
  		[ -z "${resp}" ] && conn+="No internet connection" || conn+="${resp}"
  	fi
-	echo "Connection -=- $conn"
+	echo "$(dim 'Connection -=-') $conn"
+}
+
+displayHw () {
+	local rpi
+	rpi="/proc/device-tree/model"
+ 	[ -f "$rpi" ] && echo "$(dim SBC:) $(tr -d '\0' <${rpi})" || inxi
+}
+
+displayOs () {
+	local key os
+	key="PRETTY_NAME"
+	os=$(cat /etc/os-release | grep "$key")
+	os=$(sed "s/${key}=\"/$(dim 'OS: ')/g" <<< "$os")
+	echo -e "${os::-1}"
 }
 
 ensureBash () {
@@ -65,11 +99,47 @@ getShellType () {
 	echo "${x##* }"
 }
 
+green () { # txt
+	echo -e "\e[92m${1}\e[39m"
+}
+
+invalidArgs () { # args
+	fatal "Invalid arguments $@"
+}
+
+red () { # txt
+	echo -e "\e[91m${1}\e[39m"
+}
+
+separator () {
+	for shade in {255..236}; do
+		echo -en "\e[38;5;${shade}m><><"
+	done
+	echo -e "\e[39m"
+}
+
+usage () {
+	echo "><><>< raspi-welcome $VERSION"
+	echo "usage: $0 [ascii-art-file]" 
+}
+
 ensureBash
-displayArt
-displayCmdStatus "bc" "curl" "gcc" "java" "python3" "tree" # <- list your mix here...
-echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-printf "Its $(date)\n"
-echo "Screen size: $(tput cols) x $(tput lines)"
-displayConnection
-checkTemp
+readonly VERSION="v0.2 beta"
+if [ $# -gt 1 ]; then usage
+else 
+	clear
+	displayArt "${1}"
+	separator
+	echo "$(dim 'Arch:') $(uname -m) ($(getconf LONG_BIT)-bit)"
+	checkTemp
+	displayHw
+	checkBt
+	separator
+	displayOs
+	displayCmdStatus "bc" "curl" "git" "inxi" "java" "python3" "tree" # <- list your mix here...
+	separator
+	echo -e "$(dim 'Its') $(date)"
+	echo -e "$(dim 'Screen size:') $(tput cols) x $(tput lines)"
+	displayConnection
+	separator
+fi
